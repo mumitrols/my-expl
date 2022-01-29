a1e029fc8243d5cac37650eeb42e18a5fc594211
mumitrols <mumitrols@bk.ru>
2022-01-29 13:36:44 +0300

    Изменила исходный массив значений и ожидаемый результат суммы

--- src/test/java/ru/netology/stsrv/StSrvTest.java
+++ src/test/java/ru/netology/stsrv/StSrvTest.java
@@ -8,8 +8,8 @@ class StSrvTest {
     @Test
     void testSumAllSales() {
         StSrv statService = new StSrv();
-        float[] sellings = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
-        float expected = (float) 180;
+        float[] sellings = {8, 15, 13, 15, 17, 40, 19, 20, 7, 14, 14, 18};
+        float expected = (float) 200;
         float actual = statService.sumAllSales(sellings);
         assertEquals(expected, actual);
     }
